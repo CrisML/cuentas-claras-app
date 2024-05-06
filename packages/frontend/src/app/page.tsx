e477f9c440e8745c5b529ddb2b99025d60daa653
Tomas Dahab <tomidahab@gmail.com>
2024-05-05 22:14:19 -0300

    add navbar

--- packages/frontend/src/app/page.tsx
+++ packages/frontend/src/app/page.tsx
@@ -2,7 +2,6 @@
 import { AuthProvider } from "@/app/contexts/AuthContext";
 import React from "react";
 import {Homepage} from "@/app/Homepage/Homepage";
-import { Navigate  } from "react-router-dom";
 
 export default function Home() {
 
